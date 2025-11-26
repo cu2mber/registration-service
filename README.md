9897d90f1803215995af7cb5f3546a416a5a9c04
ur <dorosi150@gmail.com>
2025-11-26 15:33:31 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# template
-프로젝트 템플릿
+# Recruitment-Service
+
+행사 모집에 참가 신청 기능을 제공하는 서비스입니다.
---
+++ src/test/java/com/cu2mber/registrationservice/RegistrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cu2mber.registrationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
